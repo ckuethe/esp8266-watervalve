fd7b0b83ffc8451b262d60a7bd750d3071afad09
Chris Kuethe <chris.kuethe+github@gmail.com>
2020-12-07 20:19:46 -0800

    Some code cleanups

--- watervalve.py
+++ watervalve.py
@@ -7,11 +7,12 @@ from time import sleep
 import network  # pylint: disable=import-error
 
 config = {
-    "led_pin": 2,  # D4  GPIO2
     "meter_pin": 12,  # D6  GPIO12
     "open_valve_pin": 14,  # D5  GPIO14
     "close_valve_pin": 16,  # D0  GPIO16
     "pulse_wait_us": 100_000,
+    "detection_time": 5,  # detection latency, in seconds
+    "flow_poll_ms": 500,  # how often to poll water flow state
 }
 
 try:
@@ -22,16 +23,18 @@ except ImportError:
     pass
 
 
-led_pin = Pin(config["led_pin"], Pin.OUT)
+try:
+    led_pin = Pin(config["led_pin"], Pin.OUT)
+except Exception:
+    led_pin = None
 close_valve_pin = Pin(config["close_valve_pin"], Pin.OUT, value=0)
 open_valve_pin = Pin(config["open_valve_pin"], Pin.OUT, value=0)
 meter_pin = Pin(config["meter_pin"], Pin.IN)
 
-response_time = 5  # detection latency, in seconds
-flow_poll_ms = 500  # how often to poll water flow state
+response_time = 5
 
 
-flow_num_samples = response_time * 1000 // flow_poll_ms
+flow_num_samples = config["detection_time"] * 1000 // config["flow_poll_ms"]
 flow_samples = []
 valve_state = None
 
@@ -48,16 +51,20 @@ class CriticalSection(object):
 
 def valve_open():
     # Simple: deassert the close_valve_pin, wait a moment, then assert the open_valve_pin
+    global valve_state
     close_valve_pin.off()
     sleep(0.01)
     open_valve_pin.on()
+    valve_state = True
 
 
 def valve_close():
     # Simple: deassert the open_valve_pin, wait a moment, then assert the close_valve_pin
+    global valve_state
     open_valve_pin.off()
     sleep(0.01)
     close_valve_pin.on()
+    valve_state = False
 
 
 def sample_flow(tmr=None):
@@ -72,12 +79,11 @@ def sample_flow(tmr=None):
         flow_samples = flow_samples[-flow_num_samples:]
 
     if tmr:
-        tmr.init(period=flow_poll_ms, mode=Timer.ONE_SHOT, callback=sample_flow)
+        tmr.init(period=config["flow_poll_ms"], mode=Timer.ONE_SHOT, callback=sample_flow)
 
 
 def valve_control(_=None):
     vsum = 0
-    global valve_state
     # with CriticalSection():
     if True:
         vsum = sum(flow_samples)
@@ -85,11 +91,11 @@ def valve_control(_=None):
     if vsum == 0 and valve_state != True:
         # no flow
         valve_open()
-        valve_state = True
+        print("opening valve")
     elif vsum == flow_num_samples and valve_state != False:
         # flow for the last... however long
         valve_close()
-        valve_state = False
+        print("closing valve")
 
 
 def network_config():
@@ -111,10 +117,11 @@ def main():
     network_config()
 
     # eye-candy: toggle LED whenever pulses are detected
-    meter_pin.irq(trigger=Pin.IRQ_FALLING, handler=lambda x: led_pin.value(led_pin.value() ^ 1))
+    if led_pin:
+        meter_pin.irq(trigger=Pin.IRQ_FALLING, handler=lambda x: led_pin.value(led_pin.value() ^ 1))
 
     flow_timer = Timer(-1)
-    flow_timer.init(period=flow_poll_ms, mode=Timer.ONE_SHOT, callback=sample_flow)
+    flow_timer.init(period=config["flow_poll_ms"], mode=Timer.ONE_SHOT, callback=sample_flow)
 
     control_timer = Timer(-1)
     control_timer.init(period=1000, mode=Timer.PERIODIC, callback=valve_control)
